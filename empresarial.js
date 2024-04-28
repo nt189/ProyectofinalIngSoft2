c5b44055edf43d8b0070daa7d82e65f0c94a53e2
Max <145935484+M4X05@users.noreply.github.com>
2024-04-28 16:36:05 -0600

    Add files via upload

---
+++ empresarial.js
@@ -0,0 +1,36 @@
+/*directorio empresarial*/
+document.getElementById("companyForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var companyName = document.getElementById("companyName").value;
+    var businessType = document.getElementById("businessType").value;
+    var companyAddress = document.getElementById("companyAddress").value;
+    var legalRepresentative = document.getElementById("legalRepresentative").value;
+    var companyPhone = document.getElementById("companyPhone").value;
+    var companyEmail = document.getElementById("companyEmail").value;
+
+    var card = document.createElement("div");
+    card.classList.add("card");
+    var deleteBtn = document.createElement("span");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerText = "Eliminar";
+    card.innerHTML = "<h2>Información recibida:</h2>" +
+                     "<p><strong>Nombre de la empresa:</strong> " + companyName + "</p>" +
+                     "<p><strong>Giro:</strong> " + businessType + "</p>" +
+                     "<p><strong>Dirección postal:</strong> " + companyAddress + "</p>" +
+                     "<p><strong>Representante legal:</strong> " + legalRepresentative + "</p>" +
+                     "<p><strong>Teléfono:</strong> " + companyPhone + "</p>" +
+                     "<p><strong>Correo electrónico:</strong> " + companyEmail + "</p>";
+    card.appendChild(deleteBtn);
+    document.getElementById("cardContainer1").appendChild(card);
+
+    document.getElementById("companyName").value = "";
+    document.getElementById("businessType").value = "";
+    document.getElementById("companyAddress").value = "";
+    document.getElementById("legalRepresentative").value = "";
+    document.getElementById("companyPhone").value = "";
+    document.getElementById("companyEmail").value = "";
+
+    deleteBtn.addEventListener("click", function() {
+        card.remove();
+    });
+});
