dcd45e636586a01b9e3a81cd4829158a6e8b55e5
Max <145935484+M4X05@users.noreply.github.com>
2024-05-06 13:46:13 -0600

    Update personal.js

--- JavaScript/personal.js
+++ JavaScript/personal.js
@@ -1,49 +1,3 @@
-// /*Directorio personal*/
-
-// document.getElementById("myForm").addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     var contactName = document.getElementById("contactName").value;
-//     var address = document.getElementById("address").value;
-//     var email = document.getElementById("email").value;
-//     var phoneHome = document.getElementById("phoneHome").value;
-//     var phoneCell = document.getElementById("phoneCell").value;
-//     var relationship = document.getElementById("relationship").value;
-
-//     /*crear tarjeta*/
-//     var card = document.createElement("div");
-//     card.classList.add("card");
-//     var deleteBtn = document.createElement("span");
-//     deleteBtn.classList.add("delete-btn");  
-//     deleteBtn.innerText = "Eliminar";   
-//     card.innerHTML = "<h2>Información de contacto:</h2>" +
-//                      "<p><strong>Nombre:</strong> " + contactName + "</p>" +
-//                      "<p><strong>Dirección postal:</strong> " + address + "</p>" +
-//                      "<p><strong>Correo electrónico:</strong> " + email + "</p>" +
-//                      "<p><strong>Teléfono particular:</strong> " + phoneHome + "</p>" +
-//                      "<p><strong>Teléfono celular:</strong> " + phoneCell + "</p>" +
-//                      "<p><strong>Parentesco:</strong> " + relationship + "</p>";
-//     card.appendChild(deleteBtn);
-//     document.getElementById("cardContainer").appendChild(card);
-
-//     /*limpiar campos */ 
-//     document.getElementById("contactName").value = "";
-//     document.getElementById("address").value = "";
-//     document.getElementById("email").value = "";
-//     document.getElementById("phoneHome").value = "";
-//     document.getElementById("phoneCell").value = "";
-//     document.getElementById("relationship").value = "";
-
-//     /*botón de eliminar*/
-//     deleteBtn.addEventListener("click", function() {
-//         card.remove();
-//     });
-// });
-
-// /*Directorio personal*/
-
-
-
-
 // Función para guardar las tarjetas en el localStorage
 function saveCardsToLocalStorage(cards) {
   localStorage.setItem('cards', JSON.stringify(cards));
@@ -110,6 +64,7 @@ function renderCards(cards) {
 
       cardElement.appendChild(saveBtn);
     });
+
     /*Agendar */
     const redirectBtn = document.createElement("button");
     redirectBtn.id = "redireccionarBtn";
@@ -184,3 +139,4 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   document.getElementById("phoneCell").value = "";
   document.getElementById("relationship").value = "";
 });
+
